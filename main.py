d131875de2d6d23c13c68cee833e454e7cba3bd9
axylious <axelbrolivas@gmail.com>
2026-04-28 06:51:57 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player(x=(SCREEN_WIDTH / 2), y=(SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
+
+    while running:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
